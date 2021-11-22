f6f9cbcf057bec16ad98feff3c0a0bf48053509b
Artem89717 <94315182+Artem89717@users.noreply.github.com>
2021-11-22 16:42:57 +0200

    Delete 3.py

--- 3.py
+++
@@ -1,4 +0,0 @@
-# Напишіть програму для сумування перших цілих n-позитивних чисел
-n = int(input("Input a number: "))
-sum_num = (n * (n + 1)) / 2
-print("Sum of the first", n, "positive integers:", sum_num)
