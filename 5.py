84be7e9f518cf1e678bb39b113ae7c1a39ec22b4
Artem89717 <94315182+Artem89717@users.noreply.github.com>
2021-11-22 16:43:14 +0200

    Delete 5.py

--- 5.py
+++
@@ -1,9 +0,0 @@
-# Напишіть програму для перетворення десяткової системи в шістнадцяткову
-arr = input().split(",")
-arr = list(map(int, arr))
-arr = list(map(hex, arr))
-arr_result = []
-for i in arr:
-    arr_result.append(i[2:])
-
-print(', '.join(arr_result))
