41a096ee20152f705468306123049572c7f04ea2
Artem89717 <94315182+Artem89717@users.noreply.github.com>
2021-11-22 16:42:42 +0200

    Delete 1.py

--- 1.py
+++
@@ -1,6 +0,0 @@
-# Написати програму, котра із введених чисел створює: список і кортеж
-values = input("Input some comma seprated numbers : ")
-list = values.split(",")
-tuple = tuple(list)
-print('List : ', list)
-print('Tuple : ', tuple)
